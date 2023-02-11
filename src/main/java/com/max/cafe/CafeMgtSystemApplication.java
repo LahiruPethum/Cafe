e2b18491643a604ae62adfddc8c23fa513c5ab2a
Lahiru Pethum <pethum013@gmail.com>
2023-02-11 20:47:05 +0530

    initial commit

---
+++ src/main/java/com/max/cafe/CafeMgtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.max.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMgtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeMgtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/max/cafe/CafeMgtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeMgtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
